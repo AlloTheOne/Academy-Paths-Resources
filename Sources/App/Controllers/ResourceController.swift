f2e4cf79dce3633f73a464d413d851dd15d6c86c
Khawlah <khawlah_alrashed@hotmail.com>
2023-03-19 11:50:38 +0300

    edit some requests in resource controller

--- Sources/App/Controllers/ResourceController.swift
+++ Sources/App/Controllers/ResourceController.swift
@@ -6,30 +6,32 @@
 //
 
 import Vapor
+import Fluent
 
 struct ResourceController: RouteCollection {
     func boot(routes: Vapor.RoutesBuilder) throws {
         let resources = routes.grouped("resources")
-        resources.on(.GET, use: getResources)
-        resources.on(.GET, ":pathName", use: getResource)
+        resources.on(.GET, use: getAllResources)
+        resources.on(.GET, ":pathName", use: getResources)
         
         resources.on(.POST, use: createResource)
-        resources.group(":resourceID") { mentor in
-            mentor.on(.DELETE, use: deleteResource)
-        }
-        resources.group(":resourceID") { mentor in
-            mentor.on(.PUT, use: updateResource)
-        }
+        resources.on(.DELETE, ":resourceID", use: deleteResource)
+        
+        resources.on(.PUT, ":resourceID", use: updateResource)
+        resources.on(.PATCH, ":resourceID", use: updateSomeFieldInResource)
     }
     
-    func getResources(req: Request) async throws -> [Resource] {
+    func getAllResources(req: Request) async throws -> [Resource] {
         try await Resource.query(on: req.db).all()
     }
     
-    func getResource(req: Request) async throws -> [Resource] {
-        let pathName = req.parameters.get("pathName")
-        // Fetches all Resources with a path name
-        return try await Resource.query(on: req.db).with(\.$path).all().filter({ $0.path.name == pathName })
+    func getResources(req: Request) async throws -> [Resource] {
+        let pathName = req.parameters.get("pathName")!
+        // Fetches all Resources with a specific path name
+        return try await Resource.query(on: req.db)
+            .join(Path.self, on: \Resource.$path.$id == \Path.$id)
+            .filter(Path.self, \.$name == pathName.capitalized)
+            .all()
     }
     
     func createResource(req: Request) async throws -> Resource {
@@ -49,13 +51,44 @@ struct ResourceController: RouteCollection {
     
     func updateResource(req: Request) async throws -> Resource {
         let input = try req.content.decode(Resource.self)
+        if let resource = try await Resource.find(req.parameters.get("resourceID"), on: req.db) {
+            resource.title = input.title
+            resource.brief = input.brief
+            resource.link = input.link
+            try await resource.update(on: req.db)
+            return resource
+        } else {
+            return try await createResource(req: req)
+        }
+    }
+    
+    func updateSomeFieldInResource(req: Request) async throws -> Resource {
         guard let resource = try await Resource.find(req.parameters.get("resourceID"), on: req.db) else {
             throw Abort(.notFound)
         }
-        resource.title = input.title
-        resource.brief = input.brief
-        resource.link = input.link
+        
+        let newResource = try req.content.decode(ResourceRequestObject.self)
+        
+        if let title = newResource.title {
+            resource.title = title
+        }
+        
+        if let brief = newResource.brief {
+            resource.brief = brief
+        }
+        
+        if let link = newResource.link {
+            resource.link = link
+        }
+        
         try await resource.save(on: req.db)
         return resource
     }
+}
+
+struct ResourceRequestObject: Content {
+    var id: UUID?
+    var title: String?
+    var brief: String?
+    var link: String?
 }
